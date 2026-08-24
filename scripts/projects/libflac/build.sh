2d419637b1306d0902c1694522d89bf97a4fa1ff
Your Name <your-email@example.com>
2026-08-24 18:11:36 +0800

    修复

--- scripts/projects/libflac/build.sh
+++ scripts/projects/libflac/build.sh
@@ -4,22 +4,8 @@ urlType="git"
 arch="aarch64 x86_64"
 buildSys="cmake"
 license="BSD-3-Clause"
-args="
-  -DBUILD_SHARED_LIBS=ON
-  -DBUILD_STATIC_LIBS=OFF
-  -DFLAC_BUILD_EXAMPLES=OFF
-  -DFLAC_BUILD_TESTS=OFF
-  -DFLAC_BUILD_DOCS=OFF
-  -DINSTALL_MANPAGES=OFF
-  -DOGG_INCLUDE_DIR=${prefix}/include
-  -DOGG_LIBRARY=${prefix}/lib/libogg.so
-  -DCMAKE_FIND_ROOT_PATH=${prefix}
-  -DICONV_INCLUDE_DIR=${prefix}/include
-  -DICONV_LIBRARY=${prefix}/lib/libiconv.so
-"
-deps="libogg libiconv"
+#args="-DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=OFF -DFLAC_BUILD_EXAMPLES=OFF -DFLAC_BUILD_TESTS=OFF -DFLAC_BUILD_DOCS=OFF -DOGG_FOUND=ON -DOGG_INCLUDE_DIR=${prefix}/include -DOGG_LIBRARY=${prefix}/lib/libogg.so -DINSTALL_MANPAGES=OFF"
+deps="libogg"
 
-pre_setup() {
-  export PKG_CONFIG_PATH="${prefix}/lib/pkgconfig:${prefix}/share/pkgconfig"
-  LDFLAGS+=" -L${prefix}/lib -liconv"
-}
+# ===== backup: original autotools build =====
+buildSys="autotools"
